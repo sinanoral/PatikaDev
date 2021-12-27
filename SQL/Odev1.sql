dd548d9f893496ade036bbd4d31667415e4385f6
sinanoral <sinnanoral@gmail.com>
2021-12-27 11:38:31 +0300

    Update Odev1.sql

--- SQL/Odev1.sql
+++ SQL/Odev1.sql
@@ -4,6 +4,6 @@ select * from film where length > 60 and length < 75
 
 select * from film where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 28.99)
 
-Smith
+-- Smith
 
 select * from film where length < 50 and rental_rate != 2.99 and rental_rate != 4.99
